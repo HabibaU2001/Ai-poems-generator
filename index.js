73ae91c64cac7c7d5a33e80c65cc3bb6cd027795
HabibaU2001 <156705727+HabibaU2001@users.noreply.github.com>
2024-03-28 23:15:25 +0000

    added basic form handling and typewriter plugin

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: ["This is a poem"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);
